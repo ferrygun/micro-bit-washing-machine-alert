34b155be9e604f5b48dd26b2d9cf61314ce6e5ef
Ferry Djaja <ferrygun@yahoo.com>
2017-06-23 16:58:50 +0800

    Update washingmachinealert.js

--- washingmachinealert.js
+++ washingmachinealert.js
@@ -1,21 +1,29 @@
 let washcycle = 0
-let now = 0
 let now1 = 0
+let now = 0
 let Xold = 0
 let Yold = 0
-let Zold = 0
+let movement = false
 let Xthreshold = 0
+let Zold = 0
 let Ythreshold = 0
 let Zthreshold = 0
+let timetostop = 0
 let Ymovement = 0
 let timetoend = 0
 let Xmovement = 0
 let timetostart = 0
 let Zmovement = 0
-let advertise = false
-let movement = false
-let timetostop = 0
+let advertise = 0
+input.onButtonPressed(Button.A, () => {
+    control.reset()
+})
+bluetooth.onBluetoothConnected(() => {
 
+})
+bluetooth.onBluetoothDisconnected(() => {
+
+})
 basic.forever(() => {
     Zmovement = input.acceleration(Dimension.Z)
     Xmovement = input.acceleration(Dimension.X)
@@ -25,7 +33,7 @@ basic.forever(() => {
             Zold = Zmovement
             movement = true
             basic.showLeds(`
-                # . . . .
+                . . . . #
                 . . . . .
                 . . . . .
                 . . . . .
@@ -38,7 +46,7 @@ basic.forever(() => {
             Yold = Ymovement
             movement = true
             basic.showLeds(`
-                . # . . .
+                . . # . .
                 . . . . .
                 . . . . .
                 . . . . .
@@ -51,7 +59,7 @@ basic.forever(() => {
             Xold = Xmovement
             movement = true
             basic.showLeds(`
-                . . # . .
+                # . . . .
                 . . . . .
                 . . . . .
                 . . . . .
@@ -64,24 +72,24 @@ basic.forever(() => {
         now1 = input.runningTime()
         if (now1 > now + timetostart) {
             if (washcycle == 0) {
-                washcycle = 1
+                washcycle = 1 // cycle started
             }
         }
     } else {
         now = input.runningTime()
         if (now > now1 + timetoend) {
             if (washcycle == 1) {
-                washcycle = 2
+                washcycle = 2 //cycle ended
             }
         }
-
         if (washcycle == 2) {
             if (now > now1 + timetostop) {
-                washcycle = 0
+                washcycle = 0 // reset cycle
+                bluetooth.stopAdvertising()
+                advertise = 0
                 basic.clearScreen()
             }
         }
-
     }
     basic.pause(500)
     basic.clearScreen()
@@ -89,26 +97,34 @@ basic.forever(() => {
         basic.showLeds(`
             . . . . .
             . . . . .
-            # # # # #
+            . . # . .
             . . . . .
             . . . . .
             `)
+        if (advertise == 0) {
+            advertise = 1
+            bluetooth.advertiseUrl(
+                "https://makecode.com",
+                7,
+                false
+            )
+        }
     }
     if (washcycle == 2) {
-        basic.showString("x")
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . #
+            `)
     }
 })
-bluetooth.onBluetoothConnected(() => {
-
-})
-bluetooth.onBluetoothDisconnected(() => {
-
-})
-advertise = false
-timetostart = 5000
-timetoend = 5 * 1000 * 60
-timetostop = 1 * 1000 * 60
+advertise = 0
+timetostart = 5000 // threshold when the cycle will start
+timetoend = 8 * 1000 * 60 //threshold when the cycle will stop
+timetostop = 1 * 1000 * 60 /// threshold when to reset to initial stage
 Zthreshold = 18
 Ythreshold = 18
 Xthreshold = 18
-bluetooth.stopAdvertising()
+
